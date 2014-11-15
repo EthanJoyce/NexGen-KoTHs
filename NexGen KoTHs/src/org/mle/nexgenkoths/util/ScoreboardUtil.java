fc9827cbf5a9979f077b72085413d047fd09975e
MrLolEthan <ethanjoyce@rocketmail.com>
2014-11-14 22:03:25 -0330

    Fixed a problem with the scoreboard.

--- NexGen KoTHs/src/org/mle/nexgenkoths/util/ScoreboardUtil.java
+++ NexGen KoTHs/src/org/mle/nexgenkoths/util/ScoreboardUtil.java
@@ -15,25 +15,42 @@ public enum ScoreboardUtil {;
     
     public static void updateScoreboard(Player player, Map<String, Integer> map) {
        Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
+       if(player.getScoreboard() != null)
+           board = player.getScoreboard();
        
-       Objective obj = board.registerNewObjective("nexgen_koths", "dummy");
-       obj.setDisplaySlot(DisplaySlot.SIDEBAR);
-       obj.setDisplayName(NexGenKoths.scoreboardObjDisplayName);
+       Objective obj = board.getObjective("nexgen_koths");
+       
+       if(obj == null) {
+           obj = board.registerNewObjective("nexgen_koths", "dummy");
+           obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+           obj.setDisplayName(NexGenKoths.scoreboardObjDisplayName);
+       } else {
+           obj.unregister();
+           
+           obj = board.registerNewObjective("nexgen_koths", "dummy");
+           obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+           obj.setDisplayName(NexGenKoths.scoreboardObjDisplayName);
+       }
        
        Map<String, Integer> globalScoreboardsMapCopy = new HashMap<String, Integer>(NexGenKoths.globalScoreboardsMap);
        for(Entry<String, Integer> entry : globalScoreboardsMapCopy.entrySet()) {
-           if(entry.getValue() != 0)
+           if(entry.getValue() > 0)
                obj.getScore(entry.getKey()).setScore(entry.getValue());
-           else
+           else {
+               board.resetScores(entry.getKey());
+               Bukkit.getServer().broadcastMessage(entry.getKey());
                NexGenKoths.globalScoreboardsMap.remove(entry.getKey());
+           }
        }
        
        if(map != null) {
            for(Entry<String, Integer> entry : map.entrySet()) {
-               if(entry.getValue() != 0)
+               if(entry.getValue() > 0)
                    obj.getScore(entry.getKey()).setScore(entry.getValue());
-               else
+               else {
+                   board.resetScores(entry.getKey());
                    map.remove(entry.getKey());
+               }
            }
        }
        
